fd90c8f327202db302336e1c057433407d930c3c
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-01-08 08:28:31 -0300

    feat: finished

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ function scrollToElement(elementSelector, instance = 0) {
 
     }
 }
-
-const link1 = document.querySelector('link')
+const link = document.getElementById('link')
+const link1 = document.getElementById('link1')
 const link2 = document.getElementById('link2')
 const link3 = document.getElementById('link3')
 
+link.addEventListener('click', () => {
+    scrollToElement('.container')
+})
 link1.addEventListener('click', () => {
+    //Scrool to the second element with "header" class
     scrollToElement('.header')
 })
 link2.addEventListener('click', () => {
-    //Scrool to the second element with "header" class
-    scrollToElement('.header', 1)
+    scrollToElement('.container-princing')
 })
 link3.addEventListener('click', () => {
-    scrollToElement('column')
+    scrollToElement('.column')
 })
